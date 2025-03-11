e3ddd54435931bf5599282b03d8a93e226eb78fe
charan <charan@charan.charan>
2025-03-11 21:59:29 +0530

    finished first chapter

---
+++ debug.h
@@ -0,0 +1,11 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include "chunk.h"
+#include "value.h"
+
+void disassemble_chunk(Chunk *chunk, const char *name);
+int disassemble_instruction(Chunk *chunk, int offset);
+void print_value(Value value);
+
+#endif
